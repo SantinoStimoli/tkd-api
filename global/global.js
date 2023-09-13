c92870c12b73ed101d4916be5668ecb376293d25
Santino Stimoli <107507086+SantinoStimoli@users.noreply.github.com>
2023-09-12 22:52:27 -0300

    global.js

---
+++ global/global.js
@@ -0,0 +1,7 @@
+export const SQL_CONFIG = {
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: '1234',
+  database: 'tkd_db'
+}
--- models/mysql/students.js
+++ models/mysql/students.js
@@ -1,14 +1,7 @@
 import mysql from 'mysql2/promise'
+import { SQL_CONFIG } from '../../global/global.js'
 
-const config = {
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: '1234',
-  database: 'tkd_db'
-}
-
-const connection = await mysql.createConnection(config)
+const connection = await mysql.createConnection(SQL_CONFIG)
 
 export class StudentsModel {
   static async getAll ({ graduation, actives }) {
